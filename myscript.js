bb9a2c03a95ad439a74fc63e062cee0bae2c283e
DJamesC <DavidJamesCumming@gmail.com>
2021-01-16 13:53:58 +0000

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+
+function generateEncounter() {
+    var encounterWrapper = document.getElementById("encounterWrapper");
+    var para = document.createElement("P");
+    para.innerHTML = 'This needs to be implimented'
+    encounterWrapper.innerHTML = '';
+    encounterWrapper.appendChild(para);
+}
